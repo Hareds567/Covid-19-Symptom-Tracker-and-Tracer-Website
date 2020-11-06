2d861b5fcd055a0ecbafcc1c0c72b4e1f4f13a42
Hareds567 <60515014+Hareds567@users.noreply.github.com>
2020-11-05 21:53:22 -0500

    new post

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ var mongoose    = require('mongoose');
 var multer      = require('multer');
 var path        = require('path');
 var csvModel    = require('./models/csv');
+var socialCircle = require('./models/socialcircle')
 var csv         = require('csvtojson');
 var bodyParser  = require('body-parser');
+
+
+
+
 
 var storage = multer.diskStorage({
     destination:(req,file,cb)=>{
@@ -16,15 +21,12 @@ var storage = multer.diskStorage({
 });
 
 var uploads = multer({storage:storage});
-
 //connect to db
 var url =   'mongodb+srv://Admin:admin@cluster0.zoibg.mongodb.net/COVID-App?retryWrites=true&w=majority'
 
 mongoose.connect(url,{useNewUrlParser:true})
 .then(()=>console.log('connected to db'))
 .catch((err)=>console.log(err))
-
-var db = mongoose.connection;
 
 
 //init app
@@ -55,7 +57,12 @@ app.get('/',(req,res)=>{
     });
 });
 
+//Random Insertions for Testing
+socialCircle.create(
+    [
+        {StudentGmail: 'nsaid.bahm@netfacc.com', First: 'yjmafiaazul12j@mixalo.com', Second: 'iliz8@mixalo.com',Third: 'hkhilawan3@fidelverta.me'}])
 
+// Handle Request
 app.post('/',uploads.single('csv'),(req,res)=>{
 csv()
 .fromFile(req.file.path)      
@@ -71,26 +78,56 @@ csv()
    });
 });
 
+app.post('/getSocialCircle',(req,res)=> {
+    var command = req.body.command;
+    var query = socialCircle.findOne({StudentGmail: command})
+    query.exec(function(err,results){
+        if(results == null){
+            res.status(200).json({
+                message: 'Null'
+            })
+        }else {
+            var searchForSocialCircle = socialCircle.find({StudentGmail: command}).select('First Second Third Fourth Fifth Sixth Seventh Eighth Ninth')
+            res.status(200).json({
+                message: searchForSocialCircle
+            })
+        }
+    })
+    //res.status(200).json({
+    //    message: "Data recieved suscessfully"
+    //});
+});
+
+app.post('/postdata',(req,res)=> {
+    var data = req.body.data;
+    res.status(200).json({
+        message: "Data recieved suscessfully"
+    });
+});
+
 //Just Playing with different queries
-var query = csvModel.find({'MajorName': 'Computer Science BA'})
-query.select('StudentEmail')
-query.exec(function(err,list){
-    console.log(list.MajorName, list.StudentEmail)
-    if (err) return handleError(err)
+
+// var query = csvModel.find({'MajorName': 'Computer Science BA'})
+// query.select('StudentEmail')
+// query.exec(function(err,list){
+//     console.log(list.MajorName, list.StudentEmail)
+//     if (err) return handleError(err)
 
    
-})
+// })
 
-var query2 = csvModel.find({}) //gets all 
+var query2 = csvModel.find({}).select('StudentEmail') //gets all 
 query2.exec(function(err,results){
-    console.log(JSON.stringify(results))
+   // results.StudentEmail
+    console.log(JSON.stringify(results.StudentEmail))
     if (err) return handleError(err)
 })
 
-var query3 = csvModel.find({'MajorName': 'Computer Science BA'}).select('StudentEmail')
+var query3 = csvModel.find({'MajorName': 'Computer Science BA'}).select('StudentEmail FirstName')
 query3.exec(function(err,data){
     console.log(JSON.stringify(data))
 })
 //assign port
 var port = process.env.PORT || 3001;
+
 app.listen(port,()=>console.log('server run at port '+ port));
