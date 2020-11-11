4537414881ff9fa90f9270c23c61829b5599d078
Hareds567 <60515014+Hareds567@users.noreply.github.com>
2020-11-11 14:18:41 -0500

    updated socialcircle model

--- models/socialcircle.js
+++ models/socialcircle.js
@@ -1,36 +1,12 @@
 var mongoose  =  require('mongoose');
 
 var socialCircleSchema = new mongoose.Schema({
-    StudentGmail:{
+    CircleUser:{
         type:String
     },
-    First:{
-        type:String
+    SocialCircle:{
+        type:Array
     },
-    Second:{
-        type:String
-    },
-    Third:{
-        type:String
-    },
-    Fourth:{
-        type:String
-    },
-    Fifth:{
-        type:String
-    },
-    Sixth:{
-        type:String
-    },
-    Seventh :{
-        type:String
-    },
-    Eighth :{
-        type:String
-    },
-    Ninth :{
-        type:String
-    }
 });
 
 module.exports = mongoose.model('SocialCircle',socialCircleSchema);
