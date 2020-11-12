a02c3f6d7b15dc72d0bf53f2a0b3e4000d96db31
Hareds567 <60515014+Hareds567@users.noreply.github.com>
2020-11-12 16:50:26 -0500

    test

--- app.js
+++ app.js
@@ -1,274 +1,324 @@
-var express     = require('express');
-var mongoose    = require('mongoose');
-var multer      = require('multer');
-var path        = require('path');
-var csvModel    = require('./models/csv');
-var socialCircle = require('./models/socialcircle')
-var csv         = require('csvtojson');
-var bodyParser  = require('body-parser');
+/**
+ * Stop using var, always use const, and if a variable should needed to be changed then use let. You should never have to use var.
+ */
 
-var storage = multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,'./public/uploads');
-    },
-    filename:(req,file,cb)=>{
-        cb(null,file.originalname);
-    }
+const express = require("express");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const path = require("path");
+const csvModel = require("./models/csv");
+const socialCircle = require("./models/socialcircle");
+const csv = require("csvtojson");
+const bodyParser = require("body-parser");
+const _ = require("lodash");
+require("dotenv").config();
+
+/**
+ *  Define requirements for express-sections and passport
+ */
+const session = require("express-session");
+const passport = require("passport");
+var userProfile;
+
+const isValid = (domain) => {
+  if (!userProfile || _.isEmpty(userProfile)) return false;
+
+  return userProfile?._json.hd === "oswego.edu";
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 
-var uploads = multer({storage:storage});
+const uploads = multer({ storage: storage });
 //connect to db
-var url =   'mongodb+srv://Admin:admin@cluster0.zoibg.mongodb.net/COVID-App?retryWrites=true&w=majority'
 
-mongoose.connect(url,{useNewUrlParser:false})
-.then(()=>console.log('connected to db'))
-.catch((err)=>console.log(err))
+const {
+  MONGO_STRING: url,
+  API_KEY: api_key,
+  API_SECRET: api_secret,
+} = process.env;
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connected to db"))
+  .catch((err) => console.log(err));
 
 //init app
-var app = express();
+const app = express();
+
+/**
+ * Tell Express to recognize sessions.
+ */
+const baseCookieOptions = {
+  name: "app.session",
+  secret: "SECRET",
+};
+
+app.use(
+  session({
+    ...baseCookieOptions,
+    resave: false,
+    saveUninitialized: true,
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  })
+);
+
+/**
+ * Tell Express to use Passport.
+ */
+app.use(passport.initialize());
+app.use(passport.session());
 
 //set the template engine
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
+
+app.set("json spaces", 2);
 
 //fetch data from the request
-app.use(bodyParser.urlencoded({extended:true})); // fixed a bug
+app.use(bodyParser.urlencoded({ extended: true })); // fixed a bug
 app.use(bodyParser.json());
 
 //static folder
-app.use(express.static(path.resolve(__dirname,'public')));
+app.use(express.static(path.resolve(__dirname, "public")));
 
 // webpage stuff
-app.get('/',(req,res)=>{
-    csvModel.find((err,data)=>{
-         if(err){
-             console.log(err);
-         }else{
-              if(data!=''){
-                  res.render('demo',{data:data});
-              }else{
-                  res.render('demo',{data:''});
-              }
-         }
+app.get("/", (req, res) => {
+  if (isValid()) return res.status(200).redirect("/dashboard");
+  else res.status(200).render("auth");
+});
+
+app.get("/dashboard", (req, res) => {
+  if (!isValid()) {
+    req.session.destroy(() => {
+      return res.status(403).redirect("/");
+    });
+  } else
+    csvModel.find((err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (data != "") {
+          res.render("demo", { data, user: userProfile });
+        } else {
+          res.render("demo", { data: "" });
+        }
+      }
     });
 });
 
-// ==============================================
+/**
+ * Passport serialization and deserialization
+ */
+passport.serializeUser((user, callback) => {
+  callback(null, user);
+});
+
+passport.deserializeUser((object, callback) => {
+  callback(null, object);
+});
+
+/**
+ * Google Authentication.
+ */
+
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const GOOGLE_CLIENT_ID = api_key;
+const GOOGLE_CLIENT_SECRET = api_secret;
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      //callbackURL: "http://localhost:3000/auth/google/callback",
+      callbackURL: "https://covidtrackerdev.herokuapp.com/",
+      hostedDomain: "oswego.edu",
+      loginHint: "You must use an oswego.eu email to access this resource!",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      userProfile = profile;
+      return done(null, userProfile);
+    }
+  )
+);
+
+/**
+ * Request callbacks
+ */
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/",
+    prompt: "select_account",
+  }),
+  (req, res) => {
+    // This only runs if the authentication was successful.
+    res.redirect("/dashboard");
+  }
+);
+// ==============================================poopybutt
 // POST: CSV upload
 // ==============================================
-app.post('/',uploads.single('csv'),(req,res)=> {
-    csv().fromFile(req.file.path).then((jsonObj)=> {
-        // does the mass insertion
-        csvModel.insertMany(jsonObj,(err,data)=> {
-            if(err) {
-                console.log(err);
-            }
-            else {
-                res.redirect('/');
-            }
-        });
-   });
+app.post("/", uploads.single("csv"), (req, res) => {
+  csv()
+    .fromFile(req.file.path)
+    .then((jsonObj) => {
+      // does the mass insertion
+      csvModel.insertMany(jsonObj, (err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.redirect("/");
+        }
+      });
+    });
 });
 
 // ==============================================
 // UNFINISEHD: trying to make a better CSV uplooad post request
 // ==============================================
-app.post('/testcsv',(req,res)=> {
-    var query_no_doc_yet = csvModel.findOne({'StudentEmail': req.body.StudentEmail})
-    query_no_doc_yet.exec(function(err,query_results){
-        if(query_results==null){ // if no document exists
-            csvModel.create( // make a new document
-                {
-                    "CourseId": [req.body.CourseId],
-                    "StudentEmail" : req.body.StudentEmail,
-                    "StudentAddress" : req.body.StudentAddress,
-                }
-            )
-            res.status(200).json({
-                message: "New document for csvdumps made"
-            })
-        } // end if
-        else {
-            csvModel.updateOne({StudentEmail:req.body.StudentEmail},  
-                {
-                    $push: {CourseId: req.body.CourseId}
-                }, function (err, docs) { 
-                if (err){ 
-                    console.log(err) 
-                } 
-                else{ 
-                    console.log("Csvdumps updated: ", docs); 
-                } 
-            })
-            res.status(200).json({
-                message: "Updated existing csvdumps doc"
-            })
-        } // end else
-    });
-})
+app.post("/testcsv", (req, res) => {
+  var query_no_doc_yet = csvModel.findOne({
+    StudentEmail: req.body.StudentEmail,
+  });
+  query_no_doc_yet.exec(function (err, query_results) {
+    if (query_results == null) {
+      // if no document exists
+      csvModel.create(
+        // make a new document
+        {
+          CourseId: [req.body.CourseId],
+          StudentEmail: req.body.StudentEmail,
+          StudentAddress: req.body.StudentAddress,
+        }
+      );
+      res.status(200).json({
+        message: "New document for csvdumps made",
+      });
+    } // end if
+    else {
+      csvModel.updateOne(
+        { StudentEmail: req.body.StudentEmail },
+        {
+          $push: { CourseId: req.body.CourseId },
+        },
+        function (err, docs) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Csvdumps updated: ", docs);
+          }
+        }
+      );
+      res.status(200).json({
+        message: "Updated existing csvdumps doc",
+      });
+    } // end else
+  });
+});
+
+// TESTING: post request, used for pinging
+app.post("/postdata", (req, res) => {
+  const data = req.body.data;
+  res.status(200).json({
+    message: "Data recieved sucessfully.",
+  });
+});
 
 // TESTING: post request, used for printing post data
-app.post('/post_test',(req,res)=> {
-    console.log('/post_test: printing req.body below')
-    console.log(req.body)
-    console.log('end /post_test')
-    res.status(200).json({
-        message: "/post_test"
-    });
+app.post("/posttest", (req, res) => {
+  var data = req.body;
+  console.log(data);
+  res.status(200).send(data);
 });
 
 // TESTING: get request, used for pinging
-app.get('/get_test',(req,res)=> {
-    console.log('/get_test: printing req.body below')
-    console.log(req.body)
-    console.log('end /get_test')
-    res.status(200).json({
-        message: "/get_test"
-    });
+app.get("/gettest", (req, res) => {
+  res.send("Get request sucessful.");
 });
 
 // ==============================================
-// OLD version, it never worked
 // GET: Social circle
 // ==============================================
-// const router = express.Router();
-// app.use("/", router);
-// router.route("/get_social_circle").get(function(req, res) {
-//     var query_getSocial = socialCircle.findOne({CircleUser: req.body.CircleUser})
-//     query_getSocial.exec(function(err,result){
-//         if(err){
-//             console.log("Error")
-//             res.send(err);
-//         }
-//         else if(result == null) {
-//             console.log('debugging req.body is below')
-//             console.log(req.body)
-//             console.log("Result was null, no social circle was found");
-//             res.send("Result was null, no social circle was found")
-//         }
-//         else{
-//             console.log("get_social_circle: sending social circle");
-//             console.log(JSON.stringify(result))
-//             res.send(result);
-//         }   
-//     }); // end query
-// });
-
-// ==============================================
-// GET: Social circle
-// ==============================================
-app.post('/get_social_circle',(req,res)=> {
-    var query_getSocial = socialCircle.findOne({CircleUser: req.body.CircleUser})
-    query_getSocial.exec(function(err,result){
-        if(err){
-            console.log("Error")
-            res.send(err);
-        }
-        else if(result == null) {
-            console.log("/get_social_circle: error null");
-            res.send("Result was null, no social circle was found")
-        }
-        else{
-            console.log("/get_social_circle: sucessful");
-            console.log(JSON.stringify(result))
-            res.send(result);
-        }   
-    }); // end query
-})
+const router = express.Router();
+app.use("/", router);
+router.route("/get_social_circle").get(function (req, res) {
+  var query_getSocial = socialCircle.findOne({
+    CircleUser: req.body.CircleUser,
+  });
+  query_getSocial.exec(function (err, result) {
+    if (err) {
+      console.log("get_social_circle: no social circle found");
+      res.send(err);
+    } else {
+      console.log("get_social_circle: get social circle");
+      console.log(JSON.stringify(result));
+      res.send(result);
+    }
+  });
+});
 
 // ==============================================
 // POST: Social circle
 // ==============================================
-app.post('/post_social_circle',(req,res)=> {
-    // DEBUGGIN
-    console.log('Justin im debugging, req.body is below')
-    console.log(req.body)
-    console.log('end debugging')
-    console.log(req.body.CircleUser)
-    // findOne will return a single document
-    var query_no_doc_yet = socialCircle.findOne({'CircleUser': req.body.CircleUser})
-    query_no_doc_yet.exec(function(err,query_results){
-        if(query_results==null){ // if no document exists
-            socialCircle.create( // make a new document
-                {
-                    "CircleUser": req.body.CircleUser,
-                    "SocialCircle" : req.body.SocialCircle
-                }
-            )
-            res.status(200).json({
-                message: "New document for social circle made"
-            })
+app.post("/post_social_circle", (req, res) => {
+  console.log(req.body.CircleUser);
+  // findOne will return a single document
+  var query_no_doc_yet = socialCircle.findOne({
+    CircleUser: req.body.CircleUser,
+  });
+  query_no_doc_yet.exec(function (err, query_results) {
+    if (query_results == null) {
+      // if no document exists
+      socialCircle.create(
+        // make a new document
+        {
+          CircleUser: req.body.CircleUser,
+          SocialCircle: req.body.SocialCircle,
         }
-        else { // document already exists, we can update existing one
-            socialCircle.updateOne({CircleUser:req.body.CircleUser},  
-                {
-                    SocialCircle: req.body.SocialCircle,
-                }, function (err, docs) { 
-                if (err){ 
-                    console.log(err) 
-                } 
-                else{ 
-                    console.log("Social circle updated: "); 
-                    // use the below, with docs, if u wanna debug
-                    //console.log("Social circle updated: ", docs); 
-                } 
-            }); 
-            res.status(200).json({
-                message: "Updated existing social circle doc"
-            })
+      );
+      res.status(200).json({
+        message: "New document for social circle made",
+      });
+    } else {
+      // document already exists, we can update existing one
+      socialCircle.updateOne(
+        { CircleUser: req.body.CircleUser },
+        {
+          SocialCircle: req.body.SocialCircle,
+        },
+        function (err, docs) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Social circle updated: ");
+            // use the below, with docs, if u wanna debug
+            //console.log("Social circle updated: ", docs);
+          }
         }
-    }); // end query
+      );
+      res.status(200).json({
+        message: "Updated existing social circle doc",
+      });
+    }
+  });
 });
 
-// ==============================================
-// Email function
-// ==============================================
-app.post('/post_send_alert',(req,res)=> {
-    console.log(req.body.Email)
-    var query_social_circle = socialCircle.findOne({'CircleUser': req.body.Email})
-    query_social_circle.exec(function(err,query_results){
-        if (err) {
-            console.log(err)
-        }
-        else if (query_results == null) {
-            console.log('Email notify error, no social circle found')
-            res.status(200).json({
-                message: "Email notify error, no social circle doc found"
-            })
-        }
-        else {
-            console.log(query_results.SocialCircle)
-            var notify_array = query_results.SocialCircle
-            var nodemailer = require('nodemailer');
-            var transporter = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                user: 'teamecovidapp@gmail.com',
-                pass: 'lepjjj2020'  //lingyu,eric,pierce,justin,jeff,john
-                }
-            });
-            
-            var mailOptions = {
-                from: 'teamecovidapp@gmail.com',
-                to: notify_array,              //change this to notify_array
-                subject: '(Testing app) You have been exposed to COVID',
-                //text: 'You should probably Quarantine or something.'   
-                html: '<h1>This is a test email for a app that is being developed, if you got this by mistake ignore this email.</h1> <h2>Someone You Have Been Close to Has Tested Positive for Covid-19</h2><p>Visit <a href="https://oswego.universitytickets.com/">here</a> to schedule an appointment for a Covid-19 test at SUNY Oswego. Covid-19 resources can be found <a href=" https://www.cdc.gov/coronavirus/2019-ncov/index.html">here</a></p>'
-            };
-            
-            transporter.sendMail(mailOptions, function(error, info){
-                if (error) {
-                console.log(error);
-                } else {
-                console.log('Email sent: ' + info.response);
-                }
-            });
-            res.status(200).json({
-                message: "Alert sent sucessfully"
-            })
-        } // end else
-    }) // end query
-}) // email post email alert
-
 //assign port
-var port = process.env.PORT || 3000;
-app.listen(port,()=>console.log('server run at port '+ port));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("server run at port " + port));
